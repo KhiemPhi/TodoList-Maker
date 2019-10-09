65871631b9fd8c138ddb95c62b03049ca26fada7
Khiem_Phi <khiemais@gmail.com>
2019-10-09 07:40:07 -0400

    Added Elements to Completed, AssignedTo and due_date

--- src/components/list_screen/ListItemCard.js
+++ src/components/list_screen/ListItemCard.js
@@ -1,24 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class ListItemCard extends Component {
-    render() {
-        return (
-            <div className='list_item_card'>
-                <div className='list_item_card_description'>
-                    {this.props.listItem.description}
-                </div>
-                <div className='list_item_card_assigned_to'>
-                    Assigned To: <strong>{this.props.listItem.assignedTo}</strong>
-                </div>
-                <div className='list_item_card_due_date'>
-                    {this.props.listItem.dueDate}
-                </div>
-                <div className='list_item_card_completed'>
-                    {this.props.listItem.completed}
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="list_item_card">
+        <div className="list_item_card_description">
+          {this.props.listItem.description}
+        </div>
+        <div className="list_item_card_assigned_to">
+          Assigned To: <strong>{this.props.listItem.assigned_to}</strong>
+        </div>
+        <div className="list_item_card_due_date">
+          {this.props.listItem.due_date}
+        </div>
+        <div
+          className={
+            this.props.listItem.completed
+              ? "list_item_card_completed"
+              : "list_item_card_not_completed"
+          } //
+        >
+          {this.props.listItem.completed ? "Completed" : "Pending"}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default ListItemCard
+export default ListItemCard;
