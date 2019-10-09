7639a3a625b2c88bb970fdda9817bd1e6daf89c2
Khiem_Phi <khiemais@gmail.com>
2019-10-09 18:25:17 -0400

    Sort By Task Implementation Incomplete

--- src/components/list_screen/ListItemsTable.js
+++ src/components/list_screen/ListItemsTable.js
@@ -2,16 +2,26 @@ import React, { Component } from "react";
 import ListItemCard from "./ListItemCard";
 
 export class ListItemsTable extends Component {
+  state = {
+    currentItemSortingCriteriaTask: true,
+    currentItemSortingCriteriaDueDate: true,
+    currentItemSortingCriteriaStatus: true
+  };
+
+  
+  sortByTask(){
+    
+  }
+  
+  
+
   
 
   render() {
     return (
       <div id="list_items_container">
         <div id="list_item_header" className="list_item_header_card">
-          <div
-            className="list_item_task_header"
-            onClick={this.sortByTask}
-          >
+          <div className="list_item_task_header" onClick={this.sortByTask }>
             Task
           </div>
           <div
@@ -20,16 +30,18 @@ export class ListItemsTable extends Component {
           >
             Due Date
           </div>
-          <div
-            className="list_item_status_header"
-            onClick={this.sortByStatus} //
-          >
+          <div className="list_item_status_header" onClick={this.sortByStatus}>
             Status
           </div>
         </div>
 
         {this.props.todoList.items.map(todoItem => (
-          <ListItemCard key={todoItem.key} listItem={todoItem} todoList={this.props.todoList} loadList = {this.props.loadList} />
+          <ListItemCard
+            key={todoItem.key}
+            listItem={todoItem}
+            todoList={this.props.todoList}
+            loadList={this.props.loadList}
+          />
         ))}
         <div className="list_item_add_card">&#x2b;</div>
       </div>
